e5535298833849416b77ba45e8800f6bc956d3a0
Samuel Beatson <svbeatson@knights.ucf.edu>
2019-10-25 15:14:40 -0400

    Added Project
    Created new project and added to repo

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    public Text hungerText;
+    public Text friendshipText;
+    public Text sickText;
+
+    public GameObject rock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hungerText.text = "I'm Hungry";
+        //friendhsipText.text = "I'm Unhappy";
+        //sickText.text = "Healthy";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StatusTextUpdate();
+    }
+
+    void StatusTextUpdate()
+    {
+        if(rock != null)
+        {
+            hungerText.text = "Hunger: " + Math.Ceiling(rock.GetComponent<RockController>().hunger).ToString();
+
+            friendshipText.text = "Friendship: " + Math.Ceiling(rock.GetComponent<RockController>().friendship).ToString();
+
+            bool isSick = rock.GetComponent<RockController>().isSick;
+            if (isSick)
+                sickText.text = "Sick";
+            else
+                sickText.text = "";
+        }
+  
+    }
+
+    public void FeedRock()
+    {
+        if (rock.GetComponent<RockController>().hunger <= 95)
+            rock.GetComponent<RockController>().hunger += 5;
+    }
+
+    public void PlayWithRock()
+    {
+        if (rock.GetComponent<RockController>().friendship <= 95)
+            rock.GetComponent<RockController>().friendship += 5;
+    }
+
+    public void GoToVet()
+    {
+        if(rock.GetComponent<RockController>().isSick)
+            rock.GetComponent<RockController>().isSick = false;
+    }
+}
---
+++ Assets/Scripts/RockController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockController : MonoBehaviour
+{
+    public string rockName = "The Boulder";
+    public float hunger = 100f;
+    public float friendship = 100f;
+    public bool isSick = false;
+
+    public float hungerScale = 2f;
+    public GameObject brokenRocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StatusUpdate();
+        CheckHealth();
+    }
+
+    void StatusUpdate()
+    {
+        // Updates Hunger and Friendship over time
+        if(isSick)
+        {
+            if (hunger > 0)
+                hunger -= Time.deltaTime * hungerScale * 3;
+            if (hunger < 90)
+                if (friendship > 0)
+                    friendship -= (90 - hunger) / 10 * Time.deltaTime * 5;
+        }
+        else
+        {
+            if (hunger > 0)
+                hunger -= Time.deltaTime * hungerScale;
+            if (hunger < 90)
+                if (friendship > 0)
+                    friendship -= (90-hunger)/10 * Time.deltaTime;
+        }
+        
+
+        // Generates a random chance for your pet rock to get sick
+        int sickChance = Random.Range(0, (int)(hunger * friendship/5));
+        if (sickChance == 667)
+            isSick = true;
+    }
+
+    void CheckHealth()
+    {
+        if (hunger <= 0)
+        {
+            Destroy(gameObject);
+            brokenRocks.SetActive(true);
+            
+        }
+        if (friendship <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
